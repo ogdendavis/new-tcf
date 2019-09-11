47ae7e41dc98c52fe5af81a48221a1e1dbc649b7
Lucas Ogden-Davis <lucas@ogdendavis.com>
2019-09-10 20:38:49 -0400

    Add components to display header menu

--- wp-content/themes/reactfit/react-src/src/components/Header.js
+++ wp-content/themes/reactfit/react-src/src/components/Header.js
@@ -1,27 +1,66 @@
 import React from 'react';
+import axios from 'axios';
 import './Header.css';
-
-// function Header() {
-//   return (
-//     <h1>Hello!</h1>
-//   )
-// }
-//
-// export default Header;
 
 class Header extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      menuItems: [],
       open: false,
     }
+
+    this.setMenu = this.setMenu.bind(this);
+  }
+
+  componentDidMount() {
+    this.setMenu();
+  }
+
+  setMenu() {
+    axios
+      .get('http://localhost/new-tcf/wp-json/reactfit/header-menu')
+      .then(response => {
+        // Just pull out needed data for now (might want more, if we get fancy)
+        const items = response.data.map(item => {
+          return {
+            id: item.ID,
+            title: item.title,
+            url: item.url,
+          };
+        });
+        // Put it in state!
+        this.setState({
+          menuItems: items,
+        });
+      })
+      .catch(error => console.log(error));
   }
 
   render() {
     return (
-      <h1>Hola!</h1>
+      <div className="header">
+        <HeaderMenu items={this.state.menuItems} />
+      </div>
     );
   }
+}
+
+const HeaderMenu = (props) => {
+  const innerContent = props.items.map(item => {
+    const itemKey = 'header-menu-' + item.id;
+    return (
+      <div className="header-menu__item" key={itemKey}>
+        <a href={item.url}>{item.title}</a>
+      </div>
+    );
+  });
+
+  return (
+    <div className="header-menu">
+      {innerContent}
+    </div>
+  );
 }
 
 export default Header
