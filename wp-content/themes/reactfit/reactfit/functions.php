3498a2577ee1c655871a0fd97924ccf8b4541ca3
Lucas Ogden-Davis <lucas@ogdendavis.com>
2019-09-20 18:25:31 -0400

    Test yarn build

---
+++ wp-content/themes/reactfit/reactfit/functions.php
@@ -0,0 +1,22 @@
+<?php
+
+function reactfit_register_menus() {
+  register_nav_menus( array(
+      'header-menu' => __( 'Header Menu' )
+    )
+  );
+}
+add_action( 'after_setup_theme', 'reactfit_register_menus' );
+
+// Need custom function to get menu item in order to send it out via REST API
+function reactfit_get_header_menu() {
+  return wp_get_nav_menu_items( get_nav_menu_locations()['header-menu'] );
+}
+
+// Creating custom endpoint for menu via REST API
+add_action( 'rest_api_init', function() {
+  register_rest_route( 'reactfit', '/header-menu', array(
+    'methods' => 'GET',
+    'callback' => 'reactfit_get_header_menu',
+  ));
+});
---
+++ wp-content/themes/reactfit/reactfit/precache-manifest.31fea61a7bdaf71a5c1eb6c7517f485f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f4f9b29dff059cfb71562ba203fa33e",
+    "url": "/new-tcf/wp-content/themes/reactfit/index.php"
+  },
+  {
+    "revision": "8fa0a26af93fa9283ad5",
+    "url": "/new-tcf/wp-content/themes/reactfit/static/css/main.chunk.css"
+  },
+  {
+    "revision": "256524c1e13187dc10f0",
+    "url": "/new-tcf/wp-content/themes/reactfit/static/js/2.chunk.js"
+  },
+  {
+    "revision": "5be984ce4dc6e2191941",
+    "url": "/new-tcf/wp-content/themes/reactfit/static/js/bundle.js"
+  },
+  {
+    "revision": "8fa0a26af93fa9283ad5",
+    "url": "/new-tcf/wp-content/themes/reactfit/static/js/main.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/new-tcf/wp-content/themes/reactfit/static/media/logo.5d5d9eef.svg"
+  }
+]);
