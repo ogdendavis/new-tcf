ce95e9cd8b87bd256bb3fab34817fc896f7447a8
Lucas Ogden-Davis <lucas@ogdendavis.com>
2019-10-14 10:43:16 -0400

    Fix testimonial arrow display in Chrome

--- wp-content/themes/reactfit/index.php
+++ wp-content/themes/reactfit/index.php
@@ -39,7 +39,7 @@
         Learn how to configure a non-root public URL by running `npm run wpbuild`.
     -->
     <title>React WordPress Theme</title>
-<link href="/new-tcf/wp-content/themes/reactfit/static/css/main.chunk.css?d9e7e43b0b9c8d1396bb" rel="stylesheet"></head>
+<link href="/new-tcf/wp-content/themes/reactfit/static/css/main.chunk.css?0013821bde418a8d30bc" rel="stylesheet"></head>
     <body>
     <noscript>
         You need to enable JavaScript to run this app.
@@ -55,5 +55,5 @@
         To begin the development, run `npm run wpstart` or `yarn wpstart`.
         To create a production bundle, use `npm run wpbuild` or `yarn wpbuild`.
     -->
-    <script src="/new-tcf/wp-content/themes/reactfit/static/js/bundle.js?d9e7e43b0b9c8d1396bb"></script><script src="/new-tcf/wp-content/themes/reactfit/static/js/0.chunk.js?d9e7e43b0b9c8d1396bb"></script><script src="/new-tcf/wp-content/themes/reactfit/static/js/main.chunk.js?d9e7e43b0b9c8d1396bb"></script></body>
+    <script src="/new-tcf/wp-content/themes/reactfit/static/js/bundle.js?0013821bde418a8d30bc"></script><script src="/new-tcf/wp-content/themes/reactfit/static/js/0.chunk.js?0013821bde418a8d30bc"></script><script src="/new-tcf/wp-content/themes/reactfit/static/js/main.chunk.js?0013821bde418a8d30bc"></script></body>
 </html>
