03609afd7c91b076517cb44b954012dc202f70e8
Lucas Ogden-Davis <lucas@ogdendavis.com>
2019-10-14 10:05:23 -0400

    Refactor trainers to be vertical

--- wp-content/themes/reactfit/react-src/src/templates/Home.js
+++ wp-content/themes/reactfit/react-src/src/templates/Home.js
@@ -143,36 +143,39 @@ const Trainers = (props) => {
       <div className="section home__trainers">
         <h2 className="section__heading">Our Trainers</h2>
 
-        <div className="trainer">
-          <div className="trainer__text">
-            <h3>Nick Sellers</h3>
-            Nick is awesome! He's so awesome that it's hard to describe how awesome he is. But let's try: He's more awesome than the awesomest thing that the awesomest person in the world could ever hope to think of.
-          </div>
-          <div className="trainer__photo">
-            <img src="http://localhost/new-tcf/wp-content/uploads/2019/10/nick-cleans.jpg" alt="Nick Sellers" />
-          </div>
-        </div>
+        <div className="trainers__container">
 
-        <div className="trainer">
-          <div className="trainer__text">
-            <h3>Abrie Sellers</h3>
-            Abrie is awesome! She's so awesome that it's hard to describe how awesome she is. But let's try: She's more awesome than the awesomest thing that the awesomest person in the world could ever hope to think of.
+          <div className="trainer">
+            <div className="trainer__photo">
+              <img src="http://localhost/new-tcf/wp-content/uploads/2019/10/nick-cleans.jpg" alt="Nick Sellers" />
+            </div>
+            <div className="trainer__text">
+              <h3>Nick Sellers</h3>
+              Nick is awesome! He's so awesome that it's hard to describe how awesome he is. But let's try: He's more awesome than the awesomest thing that the awesomest person in the world could ever hope to think of.
+            </div>
           </div>
-          <div className="trainer__photo">
-            <img src="http://localhost/new-tcf/wp-content/uploads/2019/10/abrie-square.jpg" alt="Abrie Sellers" />
-          </div>
-        </div>
 
-        <div className="trainer">
-          <div className="trainer__text">
-            <h3>Lucas Ogden-Davis</h3>
-            Lucas is ok, too. It's pretty easy to describe how ok he is. Imagine someone who is pretty ok. Now imagine that person imagining another person, who the original ok person thinks is ok. Lucas is that ok person, as envisioned by another ok person.
+          <div className="trainer">
+            <div className="trainer__photo">
+              <img src="http://localhost/new-tcf/wp-content/uploads/2019/10/abrie-square.jpg" alt="Abrie Sellers" />
+            </div>
+            <div className="trainer__text">
+              <h3>Abrie Sellers</h3>
+              Abrie is awesome! She's so awesome that it's hard to describe how awesome she is. But let's try: She's more awesome than the awesomest thing that the awesomest person in the world could ever hope to think of.
+            </div>
           </div>
-          <div className="trainer__photo">
-            <img src="http://localhost/new-tcf/wp-content/uploads/2019/10/lucas-silly.jpg" alt="Lucas Ogden-Davis" />
-          </div>
-        </div>
 
+          <div className="trainer">
+            <div className="trainer__photo">
+              <img src="http://localhost/new-tcf/wp-content/uploads/2019/10/lucas-silly.jpg" alt="Lucas Ogden-Davis" />
+            </div>
+            <div className="trainer__text">
+              <h3>Lucas Ogden-Davis</h3>
+              Lucas is ok, too. It's pretty easy to describe how ok he is. Imagine someone who is pretty ok. Now imagine that person imagining another person, who the original ok person thinks is ok. Lucas is that ok person, as envisioned by another ok person.
+            </div>
+          </div>
+
+        </div>
       </div>
     </div>
   );
