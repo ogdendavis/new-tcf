70f08eaf7f7937b4c9a7af43191dd6f194b753fe
Lucas Ogden-Davis <lucas@ogdendavis.com>
2019-11-07 13:56:11 -0500

    Change head title to site name

--- wp-content/themes/reactfit/index.php
+++ wp-content/themes/reactfit/index.php
@@ -41,8 +41,8 @@
       work correctly both with client-side routing and a non-root public URL.
       Learn how to configure a non-root public URL by running `npm run wpbuild`.
   -->
-  <title>React WordPress Theme</title>
-<link href="/new-tcf/wp-content/themes/reactfit/static/css/main.chunk.css?9e1026ac2577be168283" rel="stylesheet"></head>
+  <title><?php echo get_bloginfo('name') ?></title>
+<link href="/new-tcf/wp-content/themes/reactfit/static/css/main.chunk.css?2a1595c8aca66c6e4c83" rel="stylesheet"></head>
     <body>
     <noscript>
         You need to enable JavaScript to run this app.
@@ -58,5 +58,5 @@
         To begin the development, run `npm run wpstart` or `yarn wpstart`.
         To create a production bundle, use `npm run wpbuild` or `yarn wpbuild`.
     -->
-    <script src="/new-tcf/wp-content/themes/reactfit/static/js/bundle.js?9e1026ac2577be168283"></script><script src="/new-tcf/wp-content/themes/reactfit/static/js/0.chunk.js?9e1026ac2577be168283"></script><script src="/new-tcf/wp-content/themes/reactfit/static/js/main.chunk.js?9e1026ac2577be168283"></script></body>
+    <script src="/new-tcf/wp-content/themes/reactfit/static/js/bundle.js?2a1595c8aca66c6e4c83"></script><script src="/new-tcf/wp-content/themes/reactfit/static/js/0.chunk.js?2a1595c8aca66c6e4c83"></script><script src="/new-tcf/wp-content/themes/reactfit/static/js/main.chunk.js?2a1595c8aca66c6e4c83"></script></body>
 </html>
