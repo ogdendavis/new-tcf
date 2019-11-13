7e3ad8b165c9fd35e29acd15a8ff63e3bb8c7932
Lucas Ogden-Davis <lucas@ogdendavis.com>
2019-11-13 09:03:22 -0500

    Add Intro child component to Get Started

--- wp-content/themes/reactfit/react-src/src/templates/GetStarted.js
+++ wp-content/themes/reactfit/react-src/src/templates/GetStarted.js
@@ -8,6 +8,7 @@ import Contact from '../modules/Contact';
 // Import styles
 import './GetStarted.css';
 
+// Main export component
 class GetStarted extends React.Component {
   constructor(props) {
     super(props);
@@ -20,11 +21,21 @@ class GetStarted extends React.Component {
     return (
       <div className="get-started__wrapper">
         <Hero home={false} image={'http://localhost/new-tcf/wp-content/uploads/2019/11/learning-cleans.jpg'} />
-        <Contact home={this.state.home} id='285' addClass='get-started__form'/>
+        <Intro home={this.state.home} id='285' addClass='get-started__form'/>
         <Testimonials />
       </div>
     );
   }
+}
+
+// Stateless child components
+
+const Intro = (props) => {
+  return (
+    <div className="get-started__intro">
+      <Contact home={props.home} id={props.id} addClass={props.addClass} />
+    </div>
+  );
 }
 
 export default GetStarted;
