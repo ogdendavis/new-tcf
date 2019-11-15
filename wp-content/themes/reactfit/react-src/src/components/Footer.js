025bd526cb81ca87b23ddb240c2b859ca170801a
Lucas Ogden-Davis <lucas@ogdendavis.com>
2019-11-15 08:36:13 -0500

    Change hours in footer

--- wp-content/themes/reactfit/react-src/src/components/Footer.js
+++ wp-content/themes/reactfit/react-src/src/components/Footer.js
@@ -32,25 +32,29 @@ const FooterUpper = (props) => {
   return(
     <div className="site-footer__upper">
       <div className="site-footer__hours">
-        <h2>HOURS</h2>
+        <h2>CLASS HOURS</h2>
         <ul className="hours__list">
           <li>
             <span className="hours__day">Monday - Friday:</span>
             <ul>
-              <li>5:30am - 9:00am</li>
-              <li>3:30pm - 7:30pm</li>
+              <li>5:30a</li>
+              <li>8:00a</li>
+              <li>4:30p</li>
+              <li>5:30p</li>
+              <li>6:30p</li>
             </ul>
           </li>
           <li>
             <span className="hours__day">Saturday:</span>
             <ul>
-              <li>8:00am - 12:00pm</li>
+              <li>Open Bay<br />8:00a - 12:00p</li>
             </ul>
           </li>
           <li>
-            <span className="hours__day">Sunday:</span>
+            <span className="hours__day">Kids Class:</span>
             <ul>
-              <li>Closed</li>
+              <li>Monday & Wednesday</li>
+              <li>3:30p</li>
             </ul>
           </li>
         </ul>
