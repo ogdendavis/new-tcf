aca9fdbb060298ea2fadf93c235974a876a91a1a
Lucas Ogden-Davis <lucas@ogdendavis.com>
2019-11-15 10:01:19 -0500

    Style Contact Us template

--- wp-content/themes/reactfit/react-src/src/templates/ContactUs.js
+++ wp-content/themes/reactfit/react-src/src/templates/ContactUs.js
@@ -14,7 +14,7 @@ class ContactUs extends React.Component {
 
   render() {
     const mapEmbedCode = {
-      __html: '<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13703.936351420407!2d-83.961876!3d30.831112!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x9e2701983b333faf!2sThomasville%20CrossFit!5e0!3m2!1sen!2sus!4v1573827345631!5m2!1sen!2sus" width="400" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe>'
+      __html: '<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13703.936351420407!2d-83.961876!3d30.831112!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x9e2701983b333faf!2sThomasville%20CrossFit!5e0!3m2!1sen!2sus!4v1573827345631!5m2!1sen!2sus" width="480" height="360" frameborder="0" style="border:0;" allowfullscreen=""></iframe>'
     };
 
     return (
@@ -28,7 +28,7 @@ class ContactUs extends React.Component {
             <div className="contact-us__form">
               <h2>Send us a message</h2>
               <Contact home={this.props.home} id={this.props.formId} formClass={this.props.formClass}
-              buttonClass={'button button--no-margin'} />
+              buttonClass={'button button--alt button--no-margin'} />
             </div>
 
             <div className="contact-us__real-world">
