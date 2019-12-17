1e90e533551ecc848c352c765851f335a69bb8a0
Lucas Ogden-Davis <lucas@ogdendavis.com>
2019-12-17 13:52:28 -0500

    Style Testimonial slider

--- wp-content/themes/reactfit/react-src/src/modules/Testimonials.js
+++ wp-content/themes/reactfit/react-src/src/modules/Testimonials.js
@@ -37,23 +37,56 @@ class Testimonials extends React.Component {
   initializeGlider() {
     this.slider = new Glide('.glide');
     this.slider.mount();
+    console.log(this.state);
   }
 
   render() {
+    const width = this.props.meta.width;
+
+    const containerClass = this.props.addClass ? 'glide section__container testimonials__container ' + this.props.addClass : 'glide section__container testimonials__container';
+
+    const renderedSlides = this.state.testimonials
+        .sort((a,b) => {
+          return Number(a.acf_fields.display_order) >= Number(b.acf_fields.display_order) ? 1 : -1;
+        })
+        .map(testimonial => {
+          const img = testimonial.acf_fields.image;
+          const useImg = width < 768 ?   img.sizes.medium :
+                         width < 1024 ? img.sizes.medium_large :
+                         width < 1536 ? img.sizes.large :
+                         width < 2048 ? img.sizes['1536x1536'] :
+                         img.sizes['2048x2048'];
+
+          return (
+            <li className="glide__slide testimonial" key={'testimonial' + testimonial.acf_fields.first_name + testimonial.acf_fields.last_name}>
+              <div className="testimonial__text">
+                <div className="testimonial__quote">
+                  {testimonial.acf_fields.text}
+                </div>
+                <div className="testimonial__author">
+                  {testimonial.acf_fields.first_name}
+                </div>
+              </div>
+              <div className="testimonial__photo">
+                <img src={useImg} alt={testimonial.acf_fields.image.alt} />
+              </div>
+            </li>
+          );
+        });
+
     return (
-      <div className="glide">
+      <div className={containerClass}>
+        <h2 className="screen-reader-text">Testimonials</h2>
       {/* Slides go in glide__track */}
         <div className="glide__track" data-glide-el="track">
           <ul className="glide__slides">
-            <li className="glide__slide">0</li>
-            <li className="glide__slide">1</li>
-            <li className="glide__slide">2</li>
+            {renderedSlides}
           </ul>
         </div>
         {/* Arrow controls for slider */}
-        <div className="glide__arrows" data-glide-el="controls">
-          <button className="glide__arrow glide__arrow--left" data-glide-dir="<">prev</button>
-          <button className="glide__arrow glide__arrow--right" data-glide-dir=">">next</button>
+        <div className="glide__arrows testimonials__nav-container" data-glide-el="controls">
+          <button className="glide__arrow glide__arrow--left testimonials__nav-button" data-glide-dir="<"><NavIcon icon='caret-left' class='testimonials__arrow' /></button>
+          <button className="glide__arrow glide__arrow--right testimonials__nav-button" data-glide-dir=">"><NavIcon icon='caret-right' class='testimonials__arrow' /></button>
         </div>
       </div>
 
