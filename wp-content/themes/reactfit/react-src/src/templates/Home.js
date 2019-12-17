a191a01cade87c0d49d03429e9118fb146908d68
Lucas Ogden-Davis <lucas@ogdendavis.com>
2019-12-17 12:46:51 -0500

    Remove temporary program info from Home template

--- wp-content/themes/reactfit/react-src/src/templates/Home.js
+++ wp-content/themes/reactfit/react-src/src/templates/Home.js
@@ -17,51 +17,13 @@ class Home extends React.Component {
     this.state = {}
   }
 
-  componentDidMount() {
-    // Temporary object to hold Program info
-    const tempPrograms = [
-      {
-        title: 'CrossFit',
-        points: [
-          'Fun, motivating group classes',
-          'Led by professional CrossFit trainers',
-          'Unlimited sessions per month',
-        ],
-        button: 'Sign up now',
-        image: 'http://localhost/new-tcf/wp-content/uploads/2019/10/demo-team.jpg',
-      },
-      {
-        title: 'Foundations',
-        points: [
-          'Start CrossFit with 1:1 guidance',
-          'Learn how to eat for your goals',
-          'Gain skills and confidence',
-        ],
-        button: 'Get started',
-        image: 'http://localhost/new-tcf/wp-content/uploads/2019/10/pete-coaches.jpg',
-      },
-      {
-        title: 'Youth',
-        points: [
-          'Learn about fitness and health',
-          'Gain strength and confidence',
-          'have fun!',
-        ],
-        button: 'Learn more',
-        image: 'http://localhost/new-tcf/wp-content/uploads/2019/10/teens.jpg',
-      },
-    ];
-    this.setState({ programs: tempPrograms });
-  }
-
-
   render() {
     const fields = this.props.page.acf_fields;
 
     return (
       <div className="home__wrapper">
         <Hero image={fields.hero_image} heading={fields.hero_heading} subhead={fields.hero_subhead} />
-        <Programs meta={this.props.meta} programs={this.state.programs} />
+        <Programs meta={this.props.meta} />
         <Testimonials meta={this.props.meta}/>
         <DoIt link={fields.c2a1_button_link} text={fields.c2a1_button_text} blurb={fields.c2a1_text} />
         <Trainers meta={this.props.meta} />
