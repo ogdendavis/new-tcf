d3b706f9e1207aa5f2f67cea235fdd57d839ce9c
Lucas Ogden-Davis <lucas@ogdendavis.com>
2020-01-17 11:38:37 -0500

    Style schedule for mobile

--- wp-content/themes/reactfit/react-src/src/modules/Schedule.js
+++ wp-content/themes/reactfit/react-src/src/modules/Schedule.js
@@ -3,11 +3,15 @@ import React from 'react';
 import './Schedule.css';
 
 const Schedule = (props) => {
+  const windowWidth = window.innerWidth || document.documentElement.clientWidth;
+  const frameWidth = Math.min(windowWidth - 64, 800);
+  const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+  const frameHeight = Math.min(windowHeight - 32, 800);
   return (
     <div className="section__container">
       <div className="section schedule">
         <h2 className="section__heading">Class Schedule</h2>
-          <iframe title="Class Schedule" src={props.url} width="800" height="800"></iframe>
+          <iframe title="Class Schedule" src={props.url} width={`${frameWidth}px`} height={`${frameHeight}px`}></iframe>
       </div>
     </div>
   );
