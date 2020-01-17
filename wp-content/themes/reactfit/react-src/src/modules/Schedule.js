06aa50dfadece62e8646ea6bde64ed9fbba94f6f
Lucas Ogden-Davis <lucas@ogdendavis.com>
2020-01-17 11:27:58 -0500

    Convert schedule to stateless component

--- wp-content/themes/reactfit/react-src/src/modules/Schedule.js
+++ wp-content/themes/reactfit/react-src/src/modules/Schedule.js
@@ -2,24 +2,15 @@ import React from 'react';
 
 import './Schedule.css';
 
-class Schedule extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    }
-  }
-
-  render() {
-    return (
-      <div className="section__container">
-        <div className="section schedule">
-          <h2 className="section__heading">Class Schedule</h2>
-            <iframe title="Class Schedule" src={this.props.url} width="800" height="800"></iframe>
-        </div>
+const Schedule = (props) => {
+  return (
+    <div className="section__container">
+      <div className="section schedule">
+        <h2 className="section__heading">Class Schedule</h2>
+          <iframe title="Class Schedule" src={props.url} width="800" height="800"></iframe>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Schedule;
