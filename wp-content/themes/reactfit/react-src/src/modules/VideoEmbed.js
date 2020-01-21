e19fbb13bfb9a88d023ed78ed00a4bbfa5cc7462
Lucas Ogden-Davis <lucas@ogdendavis.com>
2020-01-21 13:34:54 -0500

    Fix video embed attributes

--- wp-content/themes/reactfit/react-src/src/modules/VideoEmbed.js
+++ wp-content/themes/reactfit/react-src/src/modules/VideoEmbed.js
@@ -5,7 +5,7 @@ const VideoEmbed = (props) => {
   const frameWidth = Math.min(windowWidth - 32, 560);
   const frameHeight = Math.floor(frameWidth * .5625)
   return (
-    <iframe title="Video Embed" width={frameWidth.toString() + 'px'} height={frameHeight.toString() + 'px'} src={props.url} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe title="Video Embed" width={frameWidth.toString() + 'px'} height={frameHeight.toString() + 'px'} src={props.url} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
   );
 }
 
